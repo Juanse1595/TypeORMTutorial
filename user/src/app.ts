7250bdb588c746f32238a7b6b3532f3373a10d6b
Juanse1595 <jspbescos15@gmail.com>
2024-05-15 15:06:51 -0500

    ADD app-data-source.ts AND entity files that define the datasource and the entity User. UPD .gitingore ignoring pnpm-lock.yaml, package.json installed express and mysql and reflect-detadata and typeorm, app.ts adding CRUD methods

--- user/src/app.ts
+++ user/src/app.ts
@@ -1,1 +1,58 @@
-console.log("App running")
+import * as express from "express"
+import { Request, Response } from "express"
+import { myDataSource } from "./app-data-source"
+import { User } from "./entity/user.entity"
+
+// Connect with data source
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source initialized!")
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initializacion:", err)
+  })
+
+
+// create and setup express app
+const app = express()
+app.use(express.json())
+
+// register routes
+
+app.get("/users", async function (req: Request, res: Response) {
+  const results = await myDataSource.getRepository(User).find()
+  res.json(results)
+})
+
+app.get("/users/:id", async function (req: Request, res: Response) {
+  const results = await myDataSource.getRepository(User).findOneBy({
+    // @ts-ignore
+    id: req.params.id,
+  })
+  return res.send(results)
+})
+
+app.post("/users", async function (req: Request, res: Response) {
+  const user = myDataSource.getRepository(User).create(req.body)
+  const results = myDataSource.getRepository(User).save(user)
+  return res.send(results)
+})
+
+app.put("/users/:id", async function (req: Request, res: Response) {
+  const userToUpdate = await myDataSource.getRepository(User).findOneBy({
+    // @ts-ignore
+    id: req.params.id,
+  })
+  myDataSource.getRepository(User).merge(userToUpdate, req.body)
+  const results = myDataSource.getRepository(User).save(userToUpdate)
+  return res.send(results)
+})
+
+app.delete("/users/:id", async function (req: Request, res: Response) {
+  const results = await myDataSource.getRepository(User).delete(req.params.id)
+  return res.send(results)
+})
+
+// start express server
+app.listen(3000, () => console.log("Server running"))
